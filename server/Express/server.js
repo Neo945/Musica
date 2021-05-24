3cfe2c376fd0a65fe4f79d6291e85749b682025b
Neo945 <shreeshsrvstv@gmail.com>
2021-05-24 15:21:45 +0530

    Establish Mongodb connection Create a basic server

---
+++ server/Express/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+mongoose.connect(process.env.ATLAS_URI,
+    {
+        useNewUrlParser:true,
+        useCreateIndex:true,
+        useUnifiedTopology: true
+    });
+mongoose.connection.once('open',()=>{
+    console.log("Connection with database established successfully");
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(require('./middleware/logger'));
+
+app.get('/',(req,res)=>{
+    res.json({message:"server is running"});
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port: ${port}`);
+});
