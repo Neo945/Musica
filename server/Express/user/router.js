98efe04f0cc0c74f511f3aa644d7f2a8717b5c8c
Neo945 <shreeshsrvstv@gmail.com>
2021-05-25 04:49:35 +0530

    Add views to perform test operations on user model

--- server/Express/user/router.js
+++ server/Express/user/router.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
+const User = require('./schema');
 const view = require('./view')
 
-router.get('/',view.response);
+// router.get('/',view.response);
+router.post('/add',view.addUser);
+router.get('/',view.getAllUser);
 
 module.exports = router;
--- server/Express/user/view.js
+++ server/Express/user/view.js
@@ -1,7 +1,24 @@
+const User = require('./schema');
+
 function response(req,res){
     res.json({message:'message from user'});
 }
 
+function addUser(req,res){
+    const username = req.body.username;
+    const nu = new User({username});
+    nu.save()
+    .then(() => res.json({message:'user added'}))
+    .catch(err => res.status(400).json('Error: ' + err));
+}
+function getAllUser(req,res){
+    User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+}
+
 module.exports = {
-    response
+    response,
+    addUser,
+    getAllUser
 };
