981ab60063d92a2f4af478809a94c28b6c001c39
Neo945 <shreeshsrvstv@gmail.com>
2021-07-08 19:49:04 +0530

    Server modified

---
+++ server/Express/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const env = require('./config/config');
+const app = require('./server')
+
+mongoose.connect(env.ATLAS_URI,
+    {
+        useNewUrlParser:true,
+        useCreateIndex:true,
+        useUnifiedTopology: true
+    });
+mongoose.connection.once('open',()=>{
+    console.log("Connection with database established successfully");
+    app.listen(env.PORT,()=>{
+        console.log(`Server is running on port: ${env.PORT}`);
+    });
+});
--- server/Express/server.js
+++ server/Express/server.js
@@ -1,27 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const path = require('path');
 const cp = require('cookie-parser');
-const env = require('./config/config');
 
 const app = express();
 
-mongoose.connect(env.ATLAS_URI,
-    {
-        useNewUrlParser:true,
-        useCreateIndex:true,
-        useUnifiedTopology: true
-    });
-mongoose.connection.once('open',()=>{
-    console.log("Connection with database established successfully");
-});
+
 
 app.use(cp());
-app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true
-}));
+app.use(cors({origin:'http://localhost:3000',credentials:true}));
 app.use(express.json());
 app.use(require('./middleware/logger'));
 app.use(require('./middleware/UserAuthetication'));
@@ -30,6 +17,4 @@ app.use('/static',express.static(path.join(__dirname,'public')));
 
 app.use('/api',require('./router'));
 
-app.listen(env.PORT,()=>{
-    console.log(`Server is running on port: ${env.PORT}`);
-});
+module.exports = app;
