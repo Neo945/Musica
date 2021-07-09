47c7c7bfdff5eb6cd69da56359718828124578d2
Neo945 <shreeshsrvstv@gmail.com>
2021-07-09 23:15:32 +0530

    Material styple in navbar

--- web-app/musica/src/components/Login/Navbar.js
+++ web-app/musica/src/components/Login/Navbar.js
@@ -1,8 +1,24 @@
 import {React,useState} from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import {AppBar,Toolbar,IconButton,Typography,Button,Drawer,List,ListItem,ListItemText} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
 
+const useStyles = makeStyles((theme) => {
+    return {
+        drawerList:{
+            width:'260px'
+        },
+        title:{
+            flexGrow:1,
+            fontFamily:"'Dancing Script', cursive",
+            fontWeight:'bold',
+            marginLeft:'25px'
+        }
+    }
+})
+
 function Navbar(params) {
+    const classes = useStyles();
     const [draw, setDraw] = useState(false);
     return (
             <AppBar position="fixed">
@@ -11,7 +27,7 @@ function Navbar(params) {
                         <MenuIcon/>
                     </IconButton>
                     <Drawer anchor={'left'} open={draw} onClose={()=>{setDraw(false)}}>
-                        <div style={{width:'260px'}}>
+                        <div className={classes.drawerList}>
                             <List>
                                 <ListItem button>
                                     <ListItemText primary="Hello world"/>
@@ -19,7 +35,7 @@ function Navbar(params) {
                             </List>
                         </div>
                     </Drawer>
-                    <Typography variant="h4" style={{'flexGrow':1,'fontFamily':"'Dancing Script', cursive",'fontWeight':'bold','marginLeft':'25px'}}>
+                    <Typography variant="h4" className={classes.title}>
                         Musica
                     </Typography>
                     <Button color="inherit" variant="h6">
