352e53d0c2bcf096f012177d9c6aea19cf14b2aa
Neo945 <shreeshsrvstv@gmail.com>
2021-08-15 16:24:31 +0530

    Add: functionality for language

--- web-app/musica/src/components/selectLanguages.js
+++ web-app/musica/src/components/selectLanguages.js
@@ -15,18 +15,10 @@ const useStyles = makeStyles((theme) => ({
       height: '25%',
       width: '100%',
       margin: theme.spacing(1),
-    //   "&:hover": {
-    //     backgroundColor: "white"
-    //   },
-    //   "&:active": {
-    //     backgroundColor: "white"
-    //   }
 },
     paper: {
       height: '100%',
       width: '100%',
-    //   background:'linear-gradient(0deg, rgba(2,173,231,0.5), rgba(2,173,231,0.5)), url(https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg) no-repeat center',
-    //   backgroundSize:'cover',
       color:'white',
       fontSize:'3em',
       fontFamily:"'Style Script', cursive",
@@ -36,23 +28,28 @@ const useStyles = makeStyles((theme) => ({
     }
   }));
 export default function LanguageSelect(params) {
-    let [Selected, setSelected] = useState([]);
-    function setState(event){
-        console.log(event.target)
-        if (Selected.indexOf(event.target.key)<0) setSelected([event.target.key,...Selected]);
-    }
+    let [selected, setSelected] = useState([]);
+    console.log(selected);
     const classes = useStyles();
     return (
         <Container component="div" maxWidth="lg" className={classes.flexCenter} style={{padding: 0,flexDirection:'column', height:'100vh'}}>
             {language.map((ele,i)=>
                 <ButtonBase
-                onClick={setState}
-                key={i} 
+                onClick={() => {
+                    if (selected.indexOf(i)<0) setSelected([i,...selected]);
+                    else {
+                        const list = [...selected];
+                        list.splice(list.indexOf(i),1);
+                        setSelected(list);
+                    };
+                }}
+                key={i}
                 className={classes.ripple}>
                     <Paper
                         style={{
-                            background:(Selected.indexOf(i)>0 ?  "linear-gradient(0deg, rgba(2,173,231,0.5), rgba(2,173,231,0.5)), url(https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg) center / cover no-repeat"
-                            : "url(https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg) center / cover no-repeat"),
+                            background:(selected.indexOf(i)>=0 ?  
+                            "linear-gradient(0deg, rgba(2,173,231,0.5), rgba(2,173,231,0.5)), url(https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg) center / cover no-repeat" : 
+                            "url(https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg) center / cover no-repeat"),
                         }}
                     className={classes.flexCenter + " " + classes.paper}>
                         {ele}
