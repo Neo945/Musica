83155bf8394d47e43364c11863b02f760a0d0b7c
Neo945 <shreeshsrvstv@gmail.com>
2021-09-16 13:33:40 +0530

    Test workflow

--- web-app/musica/src/App.js
+++ web-app/musica/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
+import "./App.css";
 // eslint-disable-next-line
-import {Login,Navbar,Signup,LanguageSelect} from './components';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
-import FormTest from './components/temp/form';
-import { makeStyles } from '@material-ui/core';
-import {Box} from '@material-ui/core'
-import PlayerPage from './components/player';
+import { Login, Navbar, Signup, LanguageSelect } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import FormTest from "./components/temp/form";
+import { makeStyles } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+import PlayerPage from "./components/player";
 
 function App() {
-  makeStyles(theme => ({
-    '@global': {
-      '#root': {
-        position: 'fixed',
+  makeStyles((theme) => ({
+    "@global": {
+      "#root": {
+        position: "fixed",
         top: 0,
         left: 0,
         right: 0,
@@ -23,29 +23,29 @@ function App() {
   }))();
   return (
     <div>
-    <Router>
-      <Navbar/>
-      <Box height="64px"/>
-      <Switch>
-        <Route path="/login">
-          <Login/>
-        </Route>
-        <Route path="/test">
-          <FormTest/>
-        </Route>
-        <Route path="/signup">
-          <Signup/>
-        </Route>
-        <Route path="/language">
-          <LanguageSelect/>
-        </Route>
-        <Route path="/player">
-          <PlayerPage />
-        </Route>
-      </Switch>
-    </Router>
+      <Router>
+        <Navbar />
+        <Box height="10%" />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/test">
+            <FormTest />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/language">
+            <LanguageSelect />
+          </Route>
+          <Route path="/player">
+            <PlayerPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  )
+  );
 }
 
 export default App;
