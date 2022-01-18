d4a86f9a38a9dae5dc46cb011e882b10d4570bc9
Neo945 <shreeshsrvstv@gmail.com>
2022-01-18 23:24:35 +0530

    Add: configuration for API autogenerated documentation

---
+++ server/Express/src/docs/swagger.config.js
@@ -0,0 +1,22 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+module.exports = (app) => {
+    const swaggerOptions = {
+        swaggerDefinition: {
+            info: {
+                version: '1.0.0',
+                title: 'Customer API',
+                description: 'Customer API Information',
+                contact: {
+                    name: 'Amazing Developer',
+                },
+                servers: ['http://localhost:5000'],
+            },
+        },
+        apis: ['src/docs/*.yml', 'src/routes/*.js', 'src/router.js'],
+    };
+
+    const swaggerDocs = swaggerJsDoc(swaggerOptions);
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+};
